a296845d6559576da7cd0c7242e7be21f662a0a2
takuya33078 <abcd1617147@outlook.jp>
2022-10-27 07:06:54 +0000

    11

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'add4cb6b9ce13854cc6d23a683460b033dc121796fd39ff4a84090d835e7fd74c0d0d4f539f7378004c3095e9ccaafb459bd93628f416d1b28681a2d01e482a7'
