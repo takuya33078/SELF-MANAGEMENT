e7353d0ba85e272b0749cc586036b5f1a50f275c
takuya33078 <abcd1617147@outlook.jp>
2022-11-23 15:31:06 +0000

    44

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -3,29 +3,32 @@ class WeightsController < ApplicationController
   
   def index
     #@weight = Weight.new(weight_params)
-    logger.debug"-------------------------------------hibihihi--------------------------"
-    logger.debug 
-    @weight = Weight.where(customer_id: nil)
+    @weight = Weight.where(customer_id: current_customer[:id])
     @weights = Weight.page(params[:page]).reverse_order
     #@weight = current_customer.weights
-    logger.debug"------------------------------------fedvdfvdv---------------------"
+    logger.debug"------------------------------------@weight---------------------"
     logger.debug(@weight.inspect)
   end
   
   def create
     #logger.debug"---------------------------------------------------------------"
     #logger.debug@params[:weight][:current_weight]
-    @customer = current_customer
-    logger.debug"---------------------------------------------------------------"
-    logger.debug @customer.inspect
-    Weight.create(current_weight: @params[:weight][:current_weight])
+    Weight.create(current_weight: @params[:weight][:current_weight], 
+                  customer_id: current_customer[:id])
+    
+    if @params[:weight][:current_weight].to_i - current_customer[:goal_weight].to_i  <= 0
+      flash[:success] = "達成おめでとうございます！"
+      redirect_to weights_path
+    else
+      flash[:success] = "登録しました。"
+      redirect_to weights_path
+    end
     #@weight = Weight.new(weight_params)
     #logger.debug"---------------------------------------------------------------"
     #logger.debug(@weight.inspect)    
     #@weight.save!
     #logger.debug"---------------------------------------------------------------"
     #logger.debug(@weight.inspect)
-    redirect_to weights_path
   end
 
   def edit
@@ -41,6 +44,12 @@ class WeightsController < ApplicationController
     @weight.update(current_weight: @params[:weight][:current_weight])
     #@weight.update(weight_params)
     #Weight.update(current_weight: @params[:current_weight], id: )
+    redirect_to weights_path
+  end
+  
+  def destroy
+    @weight = Weight.find(params[:id])
+    @weight.destroy
     redirect_to weights_path
   end
   
